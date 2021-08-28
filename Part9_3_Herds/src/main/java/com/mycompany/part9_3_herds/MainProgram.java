9eea5f3d7b6439f5d71e7d8b86447ee8ac0541f2
Santiago Delamer <80407833+Stdela@users.noreply.github.com>
2021-08-27 21:40:29 -0300

    Add files via upload

---
+++ Part9_3_Herds/src/main/java/com/mycompany/part9_3_herds/MainProgram.java
@@ -0,0 +1,19 @@
+package com.mycompany.part9_3_herds;
+
+import Part9_3.Herd;
+
+/**
+ *
+ * @author delam
+ */
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Herd herd = new Herd();
+        herd.addToHerd(new Organism(57, 66));
+        herd.addToHerd(new Organism(73, 56));
+        herd.addToHerd(new Organism(46, 52));
+        herd.addToHerd(new Organism(19, 107));
+        System.out.println(herd);
+    }
+}
---
+++ Part9_3_Herds/src/main/java/com/mycompany/part9_3_herds/Movable.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.part9_3_herds;
+
+/**
+ *
+ * @author delam
+ */
+public interface Movable {
+
+    void move(int dx, int dy);
+}
